d1fd66d16c4c516e8059c44f69569e606566d677
rkstar007 <rohankhanolkar9@gmail.com>
2023-02-22 10:19:52 +0530

    Using set function to find out unused snapshots

--- unused-snapshot.py
+++ unused-snapshot.py
@@ -1,14 +1,18 @@
 import boto3
 
-ec2_resource = boto3.resource('ec2')
+ec2 = boto3.resource('ec2', region_name='ap-south-1')
 
-# Make a list of existing volumes
-all_volumes = ec2_resource.volumes.all()
-volumes = [volume.volume_id for volume in all_volumes]
+# Retrieve a list of all the self-owned snapshots in the region
+snapshots = list(ec2.snapshots.filter(OwnerIds=['self']))
 
-# Find snapshots without existing volume
-snapshots = ec2_resource.snapshots.filter(OwnerIds=['self'])
+# Retrieve a list of all the volumes in the region
+volumes = list(ec2.volumes.all())
 
+# Create a set of all the snapshot IDs that are in use by volumes
+used_snapshots = set([snapshot.snapshot_id for volume in volumes for snapshot in volume.snapshots.all()])
+
+# Delete unused snapshots
 for snapshot in snapshots:
-    if snapshot.volume_id not in volumes:
-       delete_response = ec2_client.delete_snapshot(SnapshotId=snapshot['SnapshotId'])
+    if snapshot.snapshot_id not in used_snapshots:
+        snapshot.delete()
+        print(f"Deleted unused snapshot {snapshot.snapshot_id}")
